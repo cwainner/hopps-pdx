7af3b50ebc5f6525251160601ea289dc58d64d91
Chris Wainner <chris.jwainner@gmail.com>
2017-03-07 18:25:55 +0000

    Add menu screen with title

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
-Main = function(){
-  
-};
+function Main(){}
 
 Main.prototype = {
   preload: function(){
@@ -41,13 +39,41 @@ Main.prototype = {
       player.frame = 4;
     }
   }
-}
+};
+
+function StartMenu(){}
+
+StartMenu.prototype = {
+  init: function(){
+    this.titleText = game.make.text(game.world.centerX, 100, "Game Title", {
+      font: "bold 60pt Arial",
+      fill: "#FFFFFF",
+      align: "center"
+    });
+    this.titleText.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 5);
+    this.titleText.anchor.set(0.5);
+  },
+  
+  preload: function(){
+    
+  },
+  
+  create: function(){
+    game.stage.disableVisibilityChange = true;
+    
+    // game.add.sprite(0, 0, 'menu-bg');
+    game.add.existing(this.titleText);
+    // var txt = game.add.text(30, 280, )
+  }
+};
 
 
 $(function(){
   window.game = new Phaser.Game(800, 600, Phaser.AUTO, 'drawArea');
    
+  game.state.add('StartMenu', StartMenu);
   game.state.add('Main', Main);
-  game.state.start('Main');
+  game.state.start('StartMenu');
+  // game.state.start('Main');
    
 });
