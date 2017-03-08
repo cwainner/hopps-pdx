b6e4fadef0208272c27ad2a4c3e674e2d9b02a27
Chris Wainner <chris.jwainner@gmail.com>
2017-03-08 10:24:48 -0800

    Update main.js, add gui pause button

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,12 +1,27 @@
 function Gui(){}
 
 Gui.prototype = {
+	addMenuOption: function(text, callback){
+    var txt = game.add.text(30, game.world.height - 30, text, style.navitem.default);
+    txt.inputEnabled = true;
+    txt.events.onInputUp.add(callback);
+    txt.events.onInputOut.add(function(target){
+      target.setStyle(style.navitem.default);
+    });
+    txt.events.onInputOver.add(function(target){
+      target.setStyle(style.navitem.hover);
+    });
+    this.optionCount++;
+  },
+	
 	preload: function(){
 		
 	},
 	
 	create: function(){
-		
+		this.addMenuOption('Pause', function(e){
+			game.paused = true;
+		});
 	},
 	
 	update: function(){
